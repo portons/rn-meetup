78e9b72b4e084357da98ae4f611830bac22118b0
portons <omgrly@gmail.com>
2017-03-10 22:51:54 +0200

    Force touch + WebView demo

--- index.ios.js
+++ index.ios.js
@@ -4,11 +4,13 @@ import { DrawerNavigator } from 'react-navigation';
 import FirstScreen from './components/first-screen';
 import Map from './components/map';
 import Audio from './components/audio';
+import WebView from './components/webview';
 
 const App = DrawerNavigator({
   FirstScreen: { screen: FirstScreen },
   Map: { screen: Map },
-  Audio: { screen: Audio }
+  Audio: { screen: Audio },
+  WebView: { screen: WebView }
 });
 
 AppRegistry.registerComponent('ReactNativeMeetup', () => App);
