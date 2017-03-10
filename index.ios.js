a1f3466d1db442585816cf46c48dafcc3117fca3
portons <omgrly@gmail.com>
2017-03-10 21:49:20 +0200

    Added Map component to the drawer

--- index.ios.js
+++ index.ios.js
@@ -2,9 +2,11 @@ import { AppRegistry } from 'react-native';
 import { DrawerNavigator } from 'react-navigation';
 
 import FirstScreen from './components/first-screen';
+import Map from './components/map';
 
 const App = DrawerNavigator({
-  FirstScreen: { screen: FirstScreen }
+  FirstScreen: { screen: FirstScreen },
+  Map: { screen: Map }
 });
 
 AppRegistry.registerComponent('ReactNativeMeetup', () => App);
